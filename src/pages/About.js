a70aa826a5d8e526e8ecbeaa5603676dbfcfe1aa
Marie Drieghe <marie@statik.be>
2020-12-31 16:24:26 +0100

    circle image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import portrait from '../resources/images/self_portrait.jpeg';
 import '../styles/pages/About.css';
 import Strings from '../Strings';
 
 export default function About() {
     return (
         <div className="about-container">
-            <div className="self-portrait">
-                <img src={portrait} alt="Self Portrait"/>
-            </div>
+            <div className="self-portrait"></div>
             <div className="page-separator"/>
             <div className="about-tagline">
                 {Strings.About.tagline}
